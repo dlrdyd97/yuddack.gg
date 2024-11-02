1764e20d63744600cadc90e5767cee0456a47bd4
dlrdyd97 <dlrdyd97@naver.com>
2024-11-02 19:49:55 +0900

    123

---
+++ yuddack/src/main/java/yuddack/gg/yuddack/YuddackApplication.java
@@ -0,0 +1,13 @@
+package yuddack.gg.yuddack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuddackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuddackApplication.class, args);
+	}
+
+}
